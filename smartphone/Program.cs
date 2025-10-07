a86b945da6abbfee1f35eac122b0b9f98621ef3c
Victor Sampaio <victor.sampaio2003@gmail.com>
2025-10-06 21:57:16 -0300

    feat: Projeto finalizado

--- smartphone/Program.cs
+++ smartphone/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using smartphone.Models;
+
+Nokia tijolao = new Nokia("(31) 98988-3676", "Tijolão", "156485648", 64);
+Iphone iphone13 = new Iphone("(31) 98531-2330", "iPhone 13 Pro Max", "64584816", 256);
+
+tijolao.InstalarAplicativo("Facebook");
+iphone13.InstalarAplicativo("Instagram");
